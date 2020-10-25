0f325fc36028a14cb225170ef5ca60b9c704d2f3
shlokashah <shlokashah0603@gmail.com>
2020-10-25 13:48:26 +0530

    CRUD done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,9 +29,15 @@ class ArticlesController < ApplicationController
     		render 'edit'
   		end
 	end
- 
+ 	
+ 	def destroy
+	    @article = Article.find(params[:id])
+	    @article.destroy
+	 
+	    redirect_to articles_path
+  	end
 	private
 	  def article_params
-	    params.require(:article).permit(:title, :text)
+	   	 params.require(:article).permit(:title, :text)
 	  end
 	end
